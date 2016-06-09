b69cb68e26eab2cdc428ff218a3f5935a114237c
valiseverywhere <val_is_everywhere@yahoo.com>
2016-06-09 15:50:17 -0700

    adding affine transform function

--- curator/transforms.py
+++ curator/transforms.py
@@ -5,6 +5,5 @@ def transformAffineBatch(im,nofTransforms):
     """
         this function generates nofTransfor random affine transformations and applies them to image im
     """
-    listOfPars = [np.vstack((np.reshape(random_pars,(2,3)),[0,0,1])) for i in range(nofTransforms)]
     listOfTransformedImages = [transform.warp(im,transform.AffineTransform(rotation = np.random.uniform(0,0.1*np.pi))) for par in listOfPars]
     return(listOfTransformedImages)
